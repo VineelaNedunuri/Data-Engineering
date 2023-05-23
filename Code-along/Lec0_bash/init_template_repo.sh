260c41dfa42d37886d883ad8e9b8a5a59cec0553
vineela.nedunuri <vineela.nedunuri45@gmail.com>
2023-05-23 20:35:51 +0200

    lec0 bash code along

--- Code-along/Lec0_bash/init_template_repo.sh
+++ Code-along/Lec0_bash/init_template_repo.sh
@@ -3,6 +3,6 @@ echo "running initializations"
 sleep 1
 mkdir theory code-alongs explorations
 touch theory/.gitkeep code-alongs/.gitkeep explorations/explore.txt
-for i in  {1....5};do
-   echo "This is file $i" >file$i.txt
+for i in {1..5};do
+   echo "This is file $i" > file$i.txt
 done
