37701fa7236951b2b3b9b53408b279c999d3ace5
vineela.nedunuri <vineela.nedunuri45@gmail.com>
2023-06-07 21:32:49 +0200

    count down finished

--- Exercise1/countdown_script.py
+++ Exercise1/countdown_script.py
@@ -1,14 +1,15 @@
 import os 
-import datetime
+from datetime import datetime 
+#from tabulate import tabulate
 
 # Events with their names and dates
 events = {
-    "summer_break": datetime.datetime(2023, 6, 9, 15, 0),
-    "lia_start": datetime.datetime(2023, 9, 25, 8, 0),
-    "christmas": datetime.datetime(2023, 12, 24),
-    "bellas_birthday": datetime.datetime(2023, 12, 7),
-    "new_year": datetime.datetime(2024, 1, 1),
-    "graduation_party": datetime.datetime(2024, 6, 9, 16, 30)
+    "summer_break": datetime(2023, 6, 9, 15, 0),
+    "lia_start": datetime(2023, 9, 25, 8, 0),
+    "christmas": datetime(2023, 12, 24),
+    "bellas_birthday": datetime(2023, 12, 7),
+    "new_year": datetime(2024, 1, 1),
+    "graduation_party": datetime(2024, 6, 9, 16, 30)
 }
 
 #construct the log directory path
@@ -21,17 +22,30 @@ os.makedirs(log_dir, exist_ok=True)
 # Create the log file path
 log_path = os.path.join(log_dir, "countdown.log")
 
-current_time = datetime.datetime.now()
+current_time = datetime.now().replace(microsecond=0)
 
-for event, date in events.items():
-    time_remaining = date - current_time
+with open(log_path, "w") as file:
+    file.write(f"-------------------------------------------------\nCountdown from {current_time}\n-------------------------------------------------\n\n")
+    file.write(f"| {'Event':<20} | {'Years':<10} | {'Months':<10} | {'Days':<10} | {'Hours':<10} | {'Minutes':<10} | {'Seconds':<10} |\n")
+    file.write(f"|{'-' * 22}|{'-' * 12}|{'-' * 12}|{'-' * 12}|{'-' * 12}|{'-' * 14}|{'-' * 14}|\n")
 
-    # Calculate the breakdown of time remaining
-    years = time_remaining.days // 365
-    months = (time_remaining.days % 365) // 30
-    days = (time_remaining.days % 365) % 30
-    hours, remainder = divmod(time_remaining.seconds, 3600)
-    minutes, seconds = divmod(remainder, 60)
-    countdown = f"{event}\t{years} {months} {days} {hours} {minutes} {seconds}"   
+    print(f"-------------------------------------------------\nCountdown from {current_time}\n-------------------------------------------------\n\n")
 
-    print(countdown)
+    print(f"| {'Event':<20} | {'Years':<10} | {'Months':<10} | {'Days':<10} | {'Hours':<10} | {'Minutes':<10} | {'Seconds':<10} |")
+    print(f"|{'-' * 22}|{'-' * 12}|{'-' * 12}|{'-' * 12}|{'-' * 12}|{'-' * 12}|{'-' * 12}|")
+
+
+
+    for event, date in events.items():
+        time_remaining = date - current_time
+
+        # Calculate the breakdown of time remaining
+        years = time_remaining.days // 365
+        months = (time_remaining.days % 365) // 30
+        days = (time_remaining.days % 365) % 30
+        hours, remainder = divmod(time_remaining.seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        file.write(f"| {event:<20} | {years:<10} | {months:<10} | {days:<10} | {hours:<10} | {minutes:<10} | {seconds:<10} |\n")
+        print(f"| {event:<20} | {years:<10} | {months:<10} | {days:<10} | {hours:<10} | {minutes:<10} | {seconds:<10} |")
+
+    
