7a76364e6eff080428e5d0a9df01439bbbd42f52
vineela.nedunuri <vineela.nedunuri45@gmail.com>
2023-08-18 11:49:10 +0200

    create joke file

--- Exercise6/print_joke.py
+++ Exercise6/print_joke.py
@@ -1,10 +1,29 @@
 import pyjokes
+import random
 
 
-def print_joke():
+reactions = [
+    "Hilarious!",
+    "Oh, the humanity!",
+    "You've cracked the code!",
+    "That's comedy gold!",
+    "My sides are splitting!",
+    "Mind = blown!",
+    "Cue the laugh track!",
+    "I'm dying of laughter!",
+    "That's so bad, it's good!",
+    "*Insert uncontrollable laughter here*",
+]
+
+
+def get_random_reaction():
+    return random.choice(reactions)
+
+def print_random_joke_and_reaction():
     joke = pyjokes.get_joke()
-    print(joke)
-
+    reaction = get_random_reaction()
+    print(f"Random joke: {joke}")
+    print(f"Random reactions: {reaction}")
 
 if __name__ == "__main__":
-    print_joke()
+    print_random_joke_and_reaction()
