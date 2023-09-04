b605d4ce48a672c51e1bde195d78c27875bb2a60
vineela.nedunuri <vineela.nedunuri45@gmail.com>
2023-09-04 21:12:50 +0200

    add time stapel to joke

--- Code-along/Lec3_airflow_intro/dags/bash_operator.py
+++ Code-along/Lec3_airflow_intro/dags/bash_operator.py
@@ -12,12 +12,14 @@ data_warehouse_path = Path(__file__).parents[1] / "data" / "data_warehouse"
 
 print(data_lake_path)
 
+time_variable = "$(date +%y%m%d_%H%M%S)"
+
 with DAG(dag_id = "joke_DAG", start_date=datetime(2023,9,4)):
 
     say_hello = BashOperator(task_id = "say_hello",bash_command="echo 'hej hej joking time'")
 
     setup_folders = BashOperator(task_id = "setup_folders",bash_command= f"mkdir -p{data_lake_path} {data_warehouse_path}")
     
-    download_joke = BashOperator(task_id = "random_joke", bash_command= f"curl -o {data_lake_path} / joke.json https://official-joke-api.appspot.com/random_joke") 
+    download_joke = BashOperator(task_id = "random_joke", bash_command= f"curl -o {data_lake_path} / joke_{time_variable}.json https://official-joke-api.appspot.com/random_joke") 
 
     say_hello >> setup_folders >> download_joke
